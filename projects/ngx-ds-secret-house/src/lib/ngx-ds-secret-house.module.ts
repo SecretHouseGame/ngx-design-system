8a0aafd915d5ef79a1e897b6b85e5ef2394bec27
arty3p <gsanchez.dev@gmail.com>
2022-05-12 16:00:14 +0200

    fix (develop) : init global scss in lib / fix component in progress

--- projects/ngx-ds-secret-house/src/lib/ngx-ds-secret-house.module.ts
+++ projects/ngx-ds-secret-house/src/lib/ngx-ds-secret-house.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from "@angular/forms";
 import { NgxDsSecretHouseComponent } from './ngx-ds-secret-house.component';
 import { InputTextComponent } from './components/form/input-text/input-text.component';
+
+import { FieldContainerComponent } from "./components/form/field-container/field-container.component";
 
 @NgModule({
 	declarations: [
 		NgxDsSecretHouseComponent,
   		InputTextComponent,
+		FieldContainerComponent
 	],
-	imports: [],
+	imports: [
+		ReactiveFormsModule
+	],
 	exports: [
-		NgxDsSecretHouseComponent
+		NgxDsSecretHouseComponent,
+		FieldContainerComponent
 	]
 })
 export class NgxDsSecretHouseModule {
