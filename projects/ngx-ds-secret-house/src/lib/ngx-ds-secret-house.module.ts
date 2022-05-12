e33f9cd04b0c0b3ab8ceba01a6d0e354d61fcea7
Aimee <aimeeritleng@outlook.com>
2022-05-12 15:13:55 +0200

    fix: storybook documentation

--- projects/ngx-ds-secret-house/src/lib/ngx-ds-secret-house.module.ts
+++ projects/ngx-ds-secret-house/src/lib/ngx-ds-secret-house.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { NgxDsSecretHouseComponent } from './ngx-ds-secret-house.component';
-import { InputTextComponent } from './components/form/input-text/input-text.component';
-
 
 @NgModule({
 	declarations: [
 		NgxDsSecretHouseComponent,
-  InputTextComponent
 	],
 	imports: [],
 	exports: [
