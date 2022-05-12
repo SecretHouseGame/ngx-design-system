1da6876022a825b397474b5fdd2528c4346c28b2
Aimee <aimeeritleng@outlook.com>
2022-05-12 17:40:07 +0200

    fix: test use component wrapper in story

--- projects/ngx-ds-secret-house/src/public-api.ts
+++ projects/ngx-ds-secret-house/src/public-api.ts
@@ -5,3 +5,5 @@
 export * from './lib/ngx-ds-secret-house.service';
 export * from './lib/ngx-ds-secret-house.component';
 export * from './lib/ngx-ds-secret-house.module';
+
+export * from './lib/components/form/input-text/input-text.component';
--- stories/InputText.stories.ts
+++ stories/InputText.stories.ts
@@ -1,12 +1,24 @@
 // also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
-import { Story, Meta } from '@storybook/angular/types-6-0';
+import { Story, Meta} from '@storybook/angular/types-6-0';
+import { moduleMetadata, componentWrapperDecorator } from '@storybook/angular';
 import { InputTextComponent } from '../projects/ngx-ds-secret-house/src/lib/components/form/input-text/input-text.component';
+import { FieldContainerComponent } from '../projects/ngx-ds-secret-house/src/lib/components/form/field-container/field-container.component';
 
+import { CommonModule } from '@angular/common';
 
 // More on default export: https://storybook.js.org/docs/angular/writing-stories/introduction#default-export
 export default {
   title: 'SecretHouse/Input/Text',
-  component: InputTextComponent,
+  component: FieldContainerComponent,
+  decorators: [
+    moduleMetadata({
+      //👇 Imports both components to allow component composition with Storybook
+      declarations: [FieldContainerComponent, InputTextComponent],
+      imports: [CommonModule],
+    }),
+    //👇 Wraps our stories with a decorator
+    componentWrapperDecorator(story => `<div>${story}</div>`),
+  ],
   // More on argTypes: https://storybook.js.org/docs/angular/api/argtypes
   argTypes: {
     // backgroundColor: { control: 'color' },
@@ -16,11 +28,12 @@ export default {
 // More on component templates: https://storybook.js.org/docs/angular/writing-stories/introduction#using-args
 const Template: Story<InputTextComponent> = (args: InputTextComponent) => ({
   props: args,
+  
 });
 
-export const Primary = Template.bind({});
+export const Default = Template.bind({});
 // More on args: https://storybook.js.org/docs/angular/writing-stories/args
-Primary.args = {
+Default .args = {
   placeholder: 'Ceci est le placeholder',
   label: 'Quelle est ma question ?'
 };
