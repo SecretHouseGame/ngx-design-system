2d80ca1846d41d5ab60006d264ee548425c4d8f2
Aimee <aimeeritleng@outlook.com>
2022-05-12 15:03:34 +0200

    feat: add storybook

--- projects/ngx-ds-secret-house/src/lib/ngx-ds-secret-house.module.ts
+++ projects/ngx-ds-secret-house/src/lib/ngx-ds-secret-house.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { NgxDsSecretHouseComponent } from './ngx-ds-secret-house.component';
+import { InputTextComponent } from './components/form/input-text/input-text.component';
 
 
 @NgModule({
 	declarations: [
-		NgxDsSecretHouseComponent
+		NgxDsSecretHouseComponent,
+  InputTextComponent
 	],
 	imports: [],
 	exports: [
