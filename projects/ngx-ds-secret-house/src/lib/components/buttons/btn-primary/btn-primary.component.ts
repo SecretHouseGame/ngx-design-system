72e5c26c03875d1d207aa4ac1d54b75191330a47
arty3p <gsanchez.dev@gmail.com>
2022-05-13 11:14:38 +0200

    feat (buttons) : button primary in progress / setup font family

---
+++ projects/ngx-ds-secret-house/src/lib/components/buttons/btn-primary/btn-primary.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+	selector: 'sh-btn-primary',
+	templateUrl: './btn-primary.component.html',
+	styleUrls: ['./btn-primary.component.scss']
+})
+export class BtnPrimaryComponent implements OnInit {
+
+	constructor () {
+	}
+
+	ngOnInit (): void {
+	}
+}
