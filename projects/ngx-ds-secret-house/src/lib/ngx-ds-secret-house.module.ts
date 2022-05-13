f6ea8a6480c8ba0d7dc541fe26e38e491fd820a8
Aimee <aimeeritleng@outlook.com>
2022-05-13 10:58:55 +0200

    feat: add textarea & associated stories

--- projects/ngx-ds-secret-house/src/lib/ngx-ds-secret-house.module.ts
+++ projects/ngx-ds-secret-house/src/lib/ngx-ds-secret-house.module.ts
@@ -5,12 +5,14 @@ import { NgxDsSecretHouseComponent } from './ngx-ds-secret-house.component';
 // Forms
 import { FieldContainerComponent } from "./components/form/field-container/field-container.component";
 import { InputTextComponent } from './components/form/input-text/input-text.component';
+import { InputTextareaComponent } from './components/form/input-textarea/input-textarea.component';
 
 @NgModule({
 	declarations: [
 		NgxDsSecretHouseComponent,
 		FieldContainerComponent,
   		InputTextComponent,
+    InputTextareaComponent,
 	],
 	imports: [
 		ReactiveFormsModule
--- stories/form/InputTextarea.stories.ts
+++ stories/form/InputTextarea.stories.ts
@@ -1,19 +1,19 @@
 // also exported from '@storybook/angular' if you can deal with breaking changes in 6.1
 import { Story, Meta} from '@storybook/angular/types-6-0';
 import { moduleMetadata, componentWrapperDecorator } from '@storybook/angular';
-import { InputTextComponent } from '../projects/ngx-ds-secret-house/src/lib/components/form/input-text/input-text.component';
-import { FieldContainerComponent } from '../projects/ngx-ds-secret-house/src/lib/components/form/field-container/field-container.component';
+import { InputTextareaComponent } from '../../projects/ngx-ds-secret-house/src/lib/components/form/input-textarea/input-textarea.component';
+import { FieldContainerComponent } from '../../projects/ngx-ds-secret-house/src/lib/components/form/field-container/field-container.component';
 
 import { CommonModule } from '@angular/common';
 
 // More on default export: https://storybook.js.org/docs/angular/writing-stories/introduction#default-export
 export default {
-  title: 'SecretHouse/Input/Text',
-  component: InputTextComponent,
+  title: 'Form/Input Textarea',
+  component: InputTextareaComponent,
   decorators: [
     moduleMetadata({
       //👇 Imports both components to allow component composition with Storybook
-      declarations: [FieldContainerComponent, InputTextComponent],
+      declarations: [FieldContainerComponent, InputTextareaComponent],
       imports: [CommonModule],
     }),
     //👇 Wraps our stories with a decorator
@@ -26,7 +26,7 @@ export default {
 } as Meta;
 
 // More on component templates: https://storybook.js.org/docs/angular/writing-stories/introduction#using-args
-const Template: Story<InputTextComponent> = (args) => ({
+const Template: Story<InputTextareaComponent> = (args) => ({
   props: args,
 });
 
