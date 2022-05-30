bde41a705c682534fc79ec4d7c32d59e612500ef
arty3p <gsanchez.dev@gmail.com>
2022-05-30 16:00:08 +0200

    feat (buttons) : add loading state with overlay and rotating icon

--- projects/ngx-ds-secret-house/src/lib/components/buttons/button/button.component.ts
+++ projects/ngx-ds-secret-house/src/lib/components/buttons/button/button.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+const ioniconsUrl =
+	'https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js';
+
 @Component({
 	selector: 'sh-button',
 	templateUrl: './button.component.html',
@@ -9,11 +12,16 @@ export class ButtonComponent implements OnInit {
 	@Input() color: string = 'default';
 	@Input() outline: boolean = false;
 	@Input() disabled: boolean = false;
+	@Input() loading: boolean = false;
 
 	constructor () {
 	}
 
 	ngOnInit (): void {
+		let node = document.createElement('script');
+		node.src = ioniconsUrl;
+		node.type = 'text/javascript';
+		document.getElementsByTagName('head')[0].appendChild(node);
 	}
 
 }
