b285f71edb02190cc179b51a27482dc2b620ba5d
Romain Fréchet <romainfpro@gmail.com>
2022-05-30 17:42:08 +0200

    Style : Add notifications

--- projects/ngx-ds-secret-house/src/lib/ngx-ds-secret-house.module.ts
+++ projects/ngx-ds-secret-house/src/lib/ngx-ds-secret-house.module.ts
@@ -6,17 +6,20 @@ import { NgxDsSecretHouseComponent } from './ngx-ds-secret-house.component';
 import { FieldContainerComponent } from "./components/form/field-container/field-container.component";
 import { InputTextComponent } from './components/form/input-text/input-text.component';
 import { AlertComponent } from './components/alert/alert.component';
+import { NotificationComponent } from './components/notification/notification.component';
 
 @NgModule({
 	declarations: [
 		NgxDsSecretHouseComponent,
 		FieldContainerComponent,
   		InputTextComponent,
+		AlertComponent,
+    	NotificationComponent,
 	],
 	imports: [
 		ReactiveFormsModule,
   		InputTextComponent,
-    	AlertComponent,
+    	
 	],
 	exports: [
 		NgxDsSecretHouseComponent,
