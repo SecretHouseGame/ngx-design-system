0cb2faf3bf7e666d202212a211358162749c2bad
emelinepal <emeline43@outlook.fr>
2022-05-30 14:15:17 +0200

    nav section

--- projects/ngx-ds-secret-house/src/public-api.ts
+++ projects/ngx-ds-secret-house/src/public-api.ts
@@ -5,6 +5,8 @@
 export * from './lib/ngx-ds-secret-house.service';
 export * from './lib/ngx-ds-secret-house.component';
 export * from './lib/ngx-ds-secret-house.module';
+
+// Navigation
 export * from './lib/components/navbar/navbar.component';
 export * from './lib/components/navbar/navbar-section/navbar-section.component';
 export * from './lib/components/navbar/navbar-link/navbar-link.component';
