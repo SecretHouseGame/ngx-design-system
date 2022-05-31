ec436a697c4dd17f738fd73709d55df3223ab73b
Nicolas Notararigo (Neerfix) <nicolas.notararigo@ynov.com>
2022-05-31 17:13:53 +0200

    feat: add card and different style

---
+++ projects/ngx-ds-secret-house/src/lib/components/card/card.component.ts
@@ -0,0 +1,17 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'sh-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent implements OnInit {
+	@Input() id: string = '';
+	@Input() class: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- projects/ngx-ds-secret-house/src/lib/ngx-ds-secret-house.module.ts
+++ projects/ngx-ds-secret-house/src/lib/ngx-ds-secret-house.module.ts
@@ -26,9 +26,10 @@ import { InputFileComponent } from './components/form/input-file/input-file.comp
 import { ButtonComponent } from './components/buttons/button/button.component';
 import { ButtonActionComponent } from './components/buttons/button-action/button-action.component';
 
-// Alert & Notifications 
+// Alert & Notifications
 import { AlertComponent } from './components/alert/alert.component';
 import { NotificationComponent } from './components/notification/notification.component';
+import { CardComponent } from './components/card/card.component';
 
 const COMPONENTS = [
 	FieldContainerComponent,
@@ -52,7 +53,7 @@ const COMPONENTS = [
 ];
 
 @NgModule({
-	declarations: [NgxDsSecretHouseComponent, ...COMPONENTS],
+	declarations: [NgxDsSecretHouseComponent, ...COMPONENTS, CardComponent],
 	imports: [
 		FormsModule,
 		ReactiveFormsModule,
