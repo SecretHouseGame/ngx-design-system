634606da9014f82199451a1919d465e03e97f0b0
arty3p <gsanchez.dev@gmail.com>
2022-06-03 12:04:05 +0200

    fix (sidebar) : use props to inject links in component / update stories / export navbar interfaces

--- projects/ngx-ds-secret-house/src/lib/components/navbar/navbar.component.ts
+++ projects/ngx-ds-secret-house/src/lib/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
 import { NavbarSection } from '../../interfaces/navbar';
 
 const ioniconsUrl =
@@ -11,106 +11,7 @@ const ioniconsUrl =
 	encapsulation: ViewEncapsulation.None,
 })
 export class NavbarComponent implements OnInit {
-	navbarSections: NavbarSection[] = [
-		{
-			title: 'La maison des secrets',
-			elements: [
-				{
-					title: 'Habitants',
-					icon: 'people-outline',
-					isActive: false,
-					url: '/habitants',
-				},
-				{
-					title: 'Rooms',
-					icon: 'home-outline',
-					isActive: false,
-					url: '/rooms',
-				},
-				{
-					title: 'Buzz',
-					icon: 'hand-left-outline',
-					isActive: false,
-					url: '/buzz',
-				},
-				{
-					title: 'Secrets',
-					icon: 'eye-outline',
-					isActive: false,
-					url: '/secrets',
-				},
-			],
-		},
-		{
-			title: 'Le prime',
-			elements: [
-				{
-					title: 'Nominations',
-					icon: 'megaphone-outline',
-					isActive: false,
-					url: '/nominations',
-				},
-				{
-					title: 'Pépites de la semaine',
-					icon: 'diamond-outline',
-					isActive: false,
-					url: '/pepites',
-				},
-				{
-					title: 'Éliminations',
-					icon: 'skull-outline',
-					isActive: false,
-					url: '/eliminations',
-				},
-			],
-		},
-		{
-			title: 'Mon profil',
-			elements: [
-				{
-					title: 'Mon personnage',
-					icon: 'happy-outline',
-					isActive: false,
-					url: '/mon-personnage',
-				},
-				{
-					title: 'Mon compte',
-					icon: 'person-outline',
-					isActive: false,
-					url: '/mon-compte',
-				},
-				{
-					title: 'Paramètres',
-					icon: 'settings-outline',
-					isActive: false,
-					url: '/parametres',
-				},
-			],
-		},
-		{
-			title: 'Aide',
-			elements: [
-				{
-					title: 'Règles du jeu',
-					icon: 'information-circle-outline',
-					isActive: false,
-					url: '/regles',
-				},
-				{
-					title: 'FAQ',
-					icon: 'help-circle-outline',
-					isActive: false,
-					url: '/faq',
-				},
-				{
-					title: 'Contacter un admin',
-					icon: 'chatbubble-ellipses-outline',
-					isActive: false,
-					url: '/contacter-admin',
-				},
-			],
-		},
-	];
+	@Input() navbarSections: NavbarSection[] = [];
 
 	isOpened: boolean = false;
 
--- stories/NavBar.stories.ts
+++ stories/NavBar.stories.ts
@@ -20,5 +20,104 @@ const Template: Story<NavbarComponent> = (args: NavbarComponent) => ({
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/angular/writing-stories/args
 Primary.args = {
-
+	navbarSections: [
+		{
+			title: 'La maison des secrets',
+			elements: [
+				{
+					title: 'Habitants',
+					icon: 'people-outline',
+					isActive: false,
+					url: '/habitants',
+				},
+				{
+					title: 'Rooms',
+					icon: 'home-outline',
+					isActive: false,
+					url: '/rooms',
+				},
+				{
+					title: 'Buzz',
+					icon: 'hand-left-outline',
+					isActive: false,
+					url: '/buzz',
+				},
+				{
+					title: 'Secrets',
+					icon: 'eye-outline',
+					isActive: false,
+					url: '/secrets',
+				},
+			],
+		},
+		{
+			title: 'Le prime',
+			elements: [
+				{
+					title: 'Nominations',
+					icon: 'megaphone-outline',
+					isActive: false,
+					url: '/nominations',
+				},
+				{
+					title: 'Pépites de la semaine',
+					icon: 'diamond-outline',
+					isActive: false,
+					url: '/pepites',
+				},
+				{
+					title: 'Éliminations',
+					icon: 'skull-outline',
+					isActive: false,
+					url: '/eliminations',
+				},
+			],
+		},
+		{
+			title: 'Mon profil',
+			elements: [
+				{
+					title: 'Mon personnage',
+					icon: 'happy-outline',
+					isActive: false,
+					url: '/mon-personnage',
+				},
+				{
+					title: 'Mon compte',
+					icon: 'person-outline',
+					isActive: false,
+					url: '/mon-compte',
+				},
+				{
+					title: 'Paramètres',
+					icon: 'settings-outline',
+					isActive: false,
+					url: '/parametres',
+				},
+			],
+		},
+		{
+			title: 'Aide',
+			elements: [
+				{
+					title: 'Règles du jeu',
+					icon: 'information-circle-outline',
+					isActive: false,
+					url: '/regles',
+				},
+				{
+					title: 'FAQ',
+					icon: 'help-circle-outline',
+					isActive: false,
+					url: '/faq',
+				},
+				{
+					title: 'Contacter un admin',
+					icon: 'chatbubble-ellipses-outline',
+					isActive: false,
+					url: '/contacter-admin',
+				},
+			],
+		},
+	]
 };
